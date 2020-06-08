b2a52545046adf806148912b065573ecf536a51c
Lukas Heuberger <lukaswheuberger@gmail.com>
2020-06-08 10:12:09 +0200

    initial commit

--- app/src/main/java/com/example/plantly/MainActivity.kt
+++ app/src/main/java/com/example/plantly/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.plantly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
