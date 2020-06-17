0812060db1ae9d069ef1f810be6238b24b3b88d7
Lukas Heuberger <lukaswheuberger@gmail.com>
2020-06-17 10:24:38 +0200

    Added image view and new gray color

--- app/src/main/java/com/example/plantly/PlantFormActivity.kt
+++ app/src/main/java/com/example/plantly/PlantFormActivity.kt
@@ -3,12 +3,20 @@ package com.example.plantly
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_plant_form.*
 
 
 class PlantFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plant_form)
+
+        btnAddPlant.setOnClickListener {
+            val intent = Intent(this@PlantFormActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+        }
 
     }
 
