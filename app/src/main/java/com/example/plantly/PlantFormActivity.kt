a7c3ad9213d67fd7a50db1446df0cf681997677c
Lukas Heuberger <lukaswheuberger@gmail.com>
2020-06-22 19:55:17 +0200

    Added camera feature to the form

--- app/src/main/java/com/example/plantly/PlantFormActivity.kt
+++ app/src/main/java/com/example/plantly/PlantFormActivity.kt
@@ -1,10 +1,13 @@
 package com.example.plantly
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_plant_form.*
 
+val REQUEST_IMAGE_CAPTURE = 1
 
 class PlantFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +21,25 @@ class PlantFormActivity : AppCompatActivity() {
             overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
         }
 
+
+        ivTakePhoto.setOnClickListener {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                takePictureIntent.resolveActivity(packageManager)?.also {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            ivTakePhoto.setImageBitmap(imageBitmap)
+        }
     }
 
     override fun onBackPressed() {
